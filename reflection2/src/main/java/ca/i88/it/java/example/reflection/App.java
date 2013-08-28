bbb72b0f5a21bc427f8f75d0bf400441c588a748
i88ca <datawiki@gmail.com>
2013-08-28 14:17:27 -0400

    1st

---
+++ reflection2/src/main/java/ca/i88/it/java/example/reflection/App.java
@@ -0,0 +1,28 @@
+package ca.i88.it.java.example.reflection;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static void main(String[] args) throws InstantiationException,
+			IllegalAccessException, ClassNotFoundException,
+			NoSuchMethodException, SecurityException, IllegalArgumentException,
+			InvocationTargetException
+    {
+		Object i88ca = Class.forName("ca.i88.it.java.example.reflection.I88ca")
+				.newInstance();
+		Method m = i88ca.getClass().getDeclaredMethod("helloworld",
+				new Class<?>[0]);
+		m.invoke(i88ca);
+		m.invoke(i88ca, null);
+		m = i88ca.getClass().getDeclaredMethod("helloworld",
+				new Class<?>[] { String.class });
+
+		m.invoke(i88ca, new Object[] { "it.i88.ca" });
+    }
+}
---
+++ reflection2/src/main/java/ca/i88/it/java/example/reflection/I88ca.java
@@ -0,0 +1,13 @@
+package ca.i88.it.java.example.reflection;
+
+public class I88ca {
+
+	public void helloworld() {
+		System.out.println("Hello i88.ca!");
+	}
+
+	public void helloworld(String world) {
+		System.out.println("Hello " + world + "!");
+	}
+
+}
